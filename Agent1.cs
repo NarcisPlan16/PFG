144a741bc5d006723311515df324ca17d332a50f
Narcís <narcisplan@gmail.com>
2024-10-08 10:15:01 +0200

    Code cleaning + added DXC Award folder

--- Agent1.cs
+++ Agent1.cs
@@ -166,9 +166,17 @@ public class Agent1 : Agent {
             yield return null; // Prevent Unity freezing the frames
         }
 
+        CalcReward();
+    }
+
+    private void CalcReward(){
+        // Calculate the simulated fire reward
+
         float penalization = fire_simulation.GetReward();
         float max_pen = fires_data[n_file].total_cost;
-        float fire_reward = 1 - (penalization / max_pen); // This way the rewaard can be positive as it decreases to 0 when "penalization" approaches to "max_pen"
+
+        // This way the reward can be positive as it decreases to 0 when "penalization" approaches to "max_pen"
+        float fire_reward = 1 - (penalization / max_pen); 
         this.action_fire_reward += fire_reward;
 
         int firetrench_spent_opps = fire_simulation.FiretrenchSpentOpportunities();
